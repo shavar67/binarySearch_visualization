51eba9143704f964745accc8aba15f397962f98d
Shavar <theofficialshavar@gmail.com>
2024-05-22 11:04:36 -0400

    cleanup and improvement

--- lib/visualization/visualizer.dart
+++ lib/visualization/visualizer.dart
@@ -143,80 +143,66 @@ class _VisualizerState extends State<Visualizer> {
 }
 
 Color changeTextColor(int index, int resultValue) {
-  if (index > resultValue) {
-    //change the upperbounds of the array text color
-    return Colors.white;
-  } else if (index < resultValue) {
-    //change the lowerbounds of the array text color
-    return Colors.white;
-  } else if (index == resultValue) {
-    //highlight the index where the search value was found.
+  if (index == resultValue) {
+    // Highlight the index where the search value was found.
     return Colors.black;
   }
+  // Default color for other indices.
   return Colors.white;
 }
 
 String switchPointer(int index, int resultValue) {
-  //the index value is from the gridView itself, consider that our control point
-  //the resultValue is the value thats returned from the binarySearch impl
-
   if (index == BinarySearch.leftPointer && index != resultValue) {
-    // we only want to display the leftpointer[L],only if the control point [index]
-    //is equal to the leftPointer[L] in the BinarySearch class itself.
-    //doing so will only display the leftPointer[L] that is appropriate to (midPoint + 1).
-    //otherwise if we compare it to the resultValue all numbers to the left will have a leftPointer[L].
-
+    // Display the left pointer [L].
     return 'L';
   } else if (index == BinarySearch.rightPointer && index != resultValue) {
-    //our control point [index] is now compared to the rightPointer[R] in the BinarySearch class [mid - 1]
-    //everything is the same as above, only this time we're checking for the rightPointer[R]
+    // Display the right pointer [R].
     return 'R';
   } else if (index == resultValue && index != BinarySearch.rightPointer) {
-    //if the control point and resultValue are the same we've found our value
+    // Indicate the midpoint [M].
     return 'M';
-  } else if (index == BinarySearch.rightPointer &&
-      index == BinarySearch.leftPointer &&
-      index == (resultValue)) {
-    //return LMR when all the L, R , M pointers are the same.
+  } else if (index == BinarySearch.leftPointer &&
+      index == BinarySearch.rightPointer &&
+      index == resultValue) {
+    // Indicate that all pointers are the same [LR].
     return 'LR';
   }
-
-  //leave as is to only display L,M,R pointers,whatever character is passed here will be displayed
-  //at each index where condition isnt met above.
+  // Default is to display nothing.
   return '';
 }
 
-//The following method is simply for customization for the pointer values,
-
+//The following method is simply for customization of the pointer values,
 Color switchPointerColor(int index, int resultValue) {
   if (index == BinarySearch.leftPointer) {
+    // Color for the left pointer.
     return Colors.black;
   } else if (index == BinarySearch.rightPointer) {
+    // Color for the right pointer.
     return Colors.red;
   } else if (index == resultValue) {
+    // Color for the result index.
     return Colors.green;
   }
+  // Default color for other indices.
   return Colors.purpleAccent;
 }
-
 //The following method is simply for customization for the container background color,
 
 Color switchDefaultColor(int index, int resultValue) {
-  if (resultValue == -1) {
-    // if value we're searching for doesn't exist in our array.
-    return Colors.blueGrey;
-  } else if (index > resultValue) {
-    //the upperbounds of the array will be red
-    //the indices to the right of our result/midpoint values is greater.
-    return Colors.blue;
-  } else if (index < resultValue) {
-    //the lowerbounds of the array will be black
-    //the indices to the left of our result/midpoint values is smaller.
-    return Colors.black;
-  } else if (index == resultValue) {
-    //highlight the index and value in our array if it exists.
-    return Colors.amber;
+  switch (resultValue) {
+    case -1:
+      // Value not found in the array.
+      return Colors.blueGrey;
+    default:
+      if (index > resultValue) {
+        // Indices to the right of the result will be blue.
+        return Colors.blue;
+      } else if (index < resultValue) {
+        // Indices to the left of the result will be black.
+        return Colors.black;
+      } else {
+        // Highlight the index of the result.
+        return Colors.amber;
+      }
   }
-  //does nothing.
-  return Colors.transparent;
 }
